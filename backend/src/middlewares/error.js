71fca6df49d9edb81ac3b6c9be08ffc50464a21f
Ader817 <ader05817@gmail.com>
2026-01-03 20:25:24 +0800

    fix(api): avoid leaking internal errors

--- backend/src/middlewares/error.js
+++ backend/src/middlewares/error.js
@@ -6,7 +6,6 @@ export function errorHandler(err, req, res, next) {
   // eslint-disable-next-line no-unused-vars
   void next;
   const status = err.statusCode || err.status || 500;
-  const message = err.expose ? err.message : err.message || "Internal server error";
+  const message = err.expose ? err.message : "Internal server error";
   res.status(status).json({ message });
 }
-
